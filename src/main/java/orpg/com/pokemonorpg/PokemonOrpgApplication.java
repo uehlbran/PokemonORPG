e7eef26ea031937c062fd1f95ca065ea832ea0a9
Brandon Uehlein <uehlbran@isu.edu>
2020-04-19 15:33:09 -0600

    Initial commit.

---
+++ src/main/java/orpg/com/pokemonorpg/PokemonOrpgApplication.java
@@ -0,0 +1,13 @@
+package orpg.com.pokemonorpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonOrpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonOrpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orpg/com/pokemonorpg/PokemonOrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package orpg.com.pokemonorpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonOrpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
