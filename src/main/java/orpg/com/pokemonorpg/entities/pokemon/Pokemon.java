9a4bece45e6c01419c81d8075dc82e957be145be
Brandon Uehlein <uehlbran@isu.edu>
2020-05-06 10:38:45 -0600

    Fixed a small bug where the Pokemon class didn't have an id field declared.

--- src/main/java/orpg/com/pokemonorpg/entities/pokemon/Pokemon.java
+++ src/main/java/orpg/com/pokemonorpg/entities/pokemon/Pokemon.java
@@ -4,10 +4,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Pokemon {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
 }
